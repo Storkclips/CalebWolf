81b35945799cfd4790c54c574b1855a178554475
Josh <sitonit2@gmail.com>
2026-05-22 19:45:23 -0700

    Implement code splitting & CSS deduplication

--- src/App.jsx
+++ src/App.jsx
@@ -1,69 +1,73 @@
+import { lazy, Suspense } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import { ThemeProvider } from './contexts/ThemeContext';
-import AboutPage from './pages/AboutPage';
-import AdminPage from './pages/AdminPage';
-import AuthPage from './pages/AuthPage';
-import BlogAdminPage from './pages/BlogAdminPage';
-import BlogDetailPage from './pages/BlogDetailPage';
-import BlogEditorPage from './pages/BlogEditorPage';
-import BlogPage from './pages/BlogPage';
-import BuyCreditsPage from './pages/BuyCreditsPage';
-import CartPage from './pages/CartPage';
-import CheckoutPage from './pages/CheckoutPage';
-import ClientDownloadsPage from './pages/ClientDownloadsPage';
-import CollectionsPage from './pages/CollectionsPage';
-import ContactPage from './pages/ContactPage';
-import GalleryPage from './pages/GalleryPage';
-import ExplorePage from './pages/ExplorePage';
-import HomePage from './pages/HomePage';
-import MyLibraryPage from './pages/MyLibraryPage';
-import PricingPage from './pages/PricingPage';
-import SuccessPage from './pages/SuccessPage';
-import UnlockedGalleryPage from './pages/UnlockedGalleryPage';
-import PrintOrderSuccessPage from './pages/PrintOrderSuccessPage';
-import ScrollToTop from './components/ScrollToTop';
 import { AuthProvider } from './store/AuthContext';
 import { StoreProvider } from './store/StoreContext';
-import { Login } from './pages/Login';
-import { Signup } from './pages/Signup';
-import { ResetPassword } from './pages/ResetPassword';
+import ScrollToTop from './components/ScrollToTop';
+
+const HomePage = lazy(() => import('./pages/HomePage'));
+const Login = lazy(() => import('./pages/Login').then(m => ({ default: m.Login })));
+const Signup = lazy(() => import('./pages/Signup').then(m => ({ default: m.Signup })));
+const ResetPassword = lazy(() => import('./pages/ResetPassword').then(m => ({ default: m.ResetPassword })));
+const CollectionsPage = lazy(() => import('./pages/CollectionsPage'));
+const GalleryPage = lazy(() => import('./pages/GalleryPage'));
+const ExplorePage = lazy(() => import('./pages/ExplorePage'));
+const MyLibraryPage = lazy(() => import('./pages/MyLibraryPage'));
+const PricingPage = lazy(() => import('./pages/PricingPage'));
+const AdminPage = lazy(() => import('./pages/AdminPage'));
+const AboutPage = lazy(() => import('./pages/AboutPage'));
+const BlogPage = lazy(() => import('./pages/BlogPage'));
+const BlogAdminPage = lazy(() => import('./pages/BlogAdminPage'));
+const BlogDetailPage = lazy(() => import('./pages/BlogDetailPage'));
+const BlogEditorPage = lazy(() => import('./pages/BlogEditorPage'));
+const ContactPage = lazy(() => import('./pages/ContactPage'));
+const CartPage = lazy(() => import('./pages/CartPage'));
+const CheckoutPage = lazy(() => import('./pages/CheckoutPage'));
+const ClientDownloadsPage = lazy(() => import('./pages/ClientDownloadsPage'));
+const BuyCreditsPage = lazy(() => import('./pages/BuyCreditsPage'));
+const SuccessPage = lazy(() => import('./pages/SuccessPage'));
+const PrintOrderSuccessPage = lazy(() => import('./pages/PrintOrderSuccessPage'));
+const UnlockedGalleryPage = lazy(() => import('./pages/UnlockedGalleryPage'));
+const AuthPage = lazy(() => import('./pages/AuthPage'));
 
 export default function App() {
   return (
     <ThemeProvider>
-    <AuthProvider>
-      <StoreProvider>
-        <ScrollToTop />
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/signup" element={<Signup />} />
-          <Route path="/auth/reset-password" element={<ResetPassword />} />
-          <Route path="/collections" element={<CollectionsPage />} />
-          <Route path="/collections/:collectionId" element={<GalleryPage />} />
-          <Route path="/explore" element={<ExplorePage />} />
-          <Route path="/my-library" element={<MyLibraryPage />} />
-          <Route path="/pricing" element={<PricingPage />} />
-          <Route path="/admin" element={<AdminPage />} />
-          <Route path="/about" element={<AboutPage />} />
-          <Route path="/blog" element={<BlogPage />} />
-          <Route path="/blog/admin" element={<BlogAdminPage />} />
-          <Route path="/blog/:postId" element={<BlogDetailPage />} />
-          <Route path="/blog/new" element={<BlogEditorPage />} />
-          <Route path="/blog/:postId/edit" element={<BlogEditorPage />} />
-          <Route path="/contact" element={<ContactPage />} />
-          <Route path="/cart" element={<CartPage />} />
-          <Route path="/checkout" element={<CheckoutPage />} />
-          <Route path="/client-downloads" element={<ClientDownloadsPage />} />
-          <Route path="/buy-credits" element={<BuyCreditsPage />} />
-          <Route path="/success" element={<SuccessPage />} />
-          <Route path="/print-order-success" element={<PrintOrderSuccessPage />} />
-          <Route path="/unlocked/:collectionId" element={<UnlockedGalleryPage />} />
-          <Route path="/auth" element={<AuthPage />} />
-          <Route path="*" element={<HomePage />} />
-        </Routes>
-      </StoreProvider>
-    </AuthProvider>
+      <AuthProvider>
+        <StoreProvider>
+          <ScrollToTop />
+          <Suspense fallback={null}>
+            <Routes>
+              <Route path="/" element={<HomePage />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/signup" element={<Signup />} />
+              <Route path="/auth/reset-password" element={<ResetPassword />} />
+              <Route path="/collections" element={<CollectionsPage />} />
+              <Route path="/collections/:collectionId" element={<GalleryPage />} />
+              <Route path="/explore" element={<ExplorePage />} />
+              <Route path="/my-library" element={<MyLibraryPage />} />
+              <Route path="/pricing" element={<PricingPage />} />
+              <Route path="/admin" element={<AdminPage />} />
+              <Route path="/about" element={<AboutPage />} />
+              <Route path="/blog" element={<BlogPage />} />
+              <Route path="/blog/admin" element={<BlogAdminPage />} />
+              <Route path="/blog/:postId" element={<BlogDetailPage />} />
+              <Route path="/blog/new" element={<BlogEditorPage />} />
+              <Route path="/blog/:postId/edit" element={<BlogEditorPage />} />
+              <Route path="/contact" element={<ContactPage />} />
+              <Route path="/cart" element={<CartPage />} />
+              <Route path="/checkout" element={<CheckoutPage />} />
+              <Route path="/client-downloads" element={<ClientDownloadsPage />} />
+              <Route path="/buy-credits" element={<BuyCreditsPage />} />
+              <Route path="/success" element={<SuccessPage />} />
+              <Route path="/print-order-success" element={<PrintOrderSuccessPage />} />
+              <Route path="/unlocked/:collectionId" element={<UnlockedGalleryPage />} />
+              <Route path="/auth" element={<AuthPage />} />
+              <Route path="*" element={<HomePage />} />
+            </Routes>
+          </Suspense>
+        </StoreProvider>
+      </AuthProvider>
     </ThemeProvider>
   );
 }
